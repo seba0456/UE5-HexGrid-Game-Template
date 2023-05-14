0c37e2c9b7af3be22f72bb173da518560505008a
Sebastian Legieziński <46867564+seba0456@users.noreply.github.com>
2023-05-14 23:40:56 +0200

    update
    minor update

--- Source/HexGrid/Public/TileOperations.h
+++ Source/HexGrid/Public/TileOperations.h
@@ -16,7 +16,7 @@ class HEXGRID_API UTileOperations : public UBlueprintFunctionLibrary
         UFUNCTION(BlueprintCallable, Category = "Tile Operations")
         static TArray<FIntPoint> GetTileNeighboursInRange(const FIntPoint& SourceTile, int Radius);
         
-    UFUNCTION(BlueprintCallable, Category = "Tile Operations", meta = (DisplayName = "Get Adjacent Tile Neighbours test", Description = "A function that returns tile neighbours."))
+    UFUNCTION(BlueprintCallable, Category = "Tile Operations", meta = (DisplayName = "Get Adjacent Tile Neighbours", Description = "A function that returns tile neighbours."))
         static TArray<FIntPoint> GetAdjacentTileNeighbours(const FIntPoint& SourceTile);
 
 };
